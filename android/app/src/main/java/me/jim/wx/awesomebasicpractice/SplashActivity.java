3e49ac103572848c91c7c774aa0ac6dcb177b3af
weix <weix@inke.cn>
2017-12-14 00:04:40 +0800

    ReactNative集成到现有Android 项目中

---
+++ android/app/src/main/java/me/jim/wx/awesomebasicpractice/SplashActivity.java
@@ -0,0 +1,21 @@
+package me.jim.wx.awesomebasicpractice;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import me.jim.wx.awesomebasicpractice.reactnative.MyReactActivity;
+import me.jim.wx.awesomebasicpractice.view.primary.FlowLayout;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Intent intent = new Intent(this, MyReactActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
