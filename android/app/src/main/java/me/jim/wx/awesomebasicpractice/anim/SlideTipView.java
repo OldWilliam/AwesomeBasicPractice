11c78f90a65786ab2e73afa058ee5768ecac31d6
weix <weix@inke.cn>
2018-04-16 15:42:15 +0800

    悬浮窗侧边隐藏动画

---
+++ android/app/src/main/java/me/jim/wx/awesomebasicpractice/anim/SlideTipView.java
@@ -0,0 +1,10 @@
+package me.jim.wx.awesomebasicpractice.anim;
+
+import android.widget.LinearLayout;
+
+/**
+ * Created by wx on 2018/4/16.
+ */
+
+public class SlideTipView extends LinearLayout {
+}
