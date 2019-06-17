7ac37c0771e2fefc5d0998878d718f0288a622d3
weix <weix@inke.cn>
2019-06-17 18:19:59 +0800

    java 并发示例

---
+++ android/JavaConcurrencyModule/src/main/java/me/jim/wx/javamodule/Counter.java
@@ -0,0 +1,58 @@
+package me.jim.wx.javamodule;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Date: 2019/6/17
+ * Name: wx
+ * Description: 线程安全的计数器
+ */
+public class Counter {
+    private int i = 0;
+    private AtomicInteger atomI = new AtomicInteger(0);
+
+    public static void main(String[] args) {
+        ArrayList<Thread> threads = new ArrayList<>();
+        final Counter counter = new Counter();
+        for (int i = 0; i < 1000; i++) {
+            threads.add(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int j = 0; j < 1000; j++) {
+                        counter.count();
+                        counter.safeCount();
+                    }
+                }
+            }));
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(counter.i + " " +counter.atomI.get());
+    }
+
+    private void safeCount() {
+        while (true) {
+            int i = atomI.get();
+            boolean success = atomI.compareAndSet(i, ++i);
+            if (success) {
+                break;
+            }
+        }
+    }
+
+    private void count() {
+        i++;
+    }
+}
